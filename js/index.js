2e0dbf67eecf41e070fab7b2052768e384710baa
duruozdemir <ozdemir.duruuu@gmail.com>
2024-09-09 12:33:47 +0300

    duru/BE-9-footer-section

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active');
+
+        const content = button.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+});
