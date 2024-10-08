c797196a7c4c10a988fb6ede7e379af67b954ab2
Cerenucakb <cerenucak.b@gmail.com>
2024-10-08 15:34:14 +0300

    [BE-12] - Create-Trending-Movies-Section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Cerenucakb/BE-12-Create-Trending-Movies-Section
 const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjU2MDg1M2Y5OWVhM2QxNTAwMjRkYTY0MzU0NjJjNiIsIm5iZiI6MTcyNjE3OTkyNy4wMjg2OTYsInN1YiI6IjY2ZTM1NzllOTAxM2ZlODcyMjIzYmZlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VE4_B-sg_0yz557YA98_GtbwS_ndY8fEz2SdFFNYyHA';
 const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 
@@ -7,16 +8,25 @@ let movies = [];
 
 document.querySelector('.control-right-btn').addEventListener('click', () => {
     currentSlide++;
-    if (currentSlide > movies.length - 1) currentSlide = movies.length - 1;  
+    if (currentSlide > movies.length)  currentSlide = 0;  
     slider.style.transform = `translateX(-${currentSlide * 850}px)`;   
     addMovieToSlider(currentSlide);  
 });
 
 document.querySelector('.control-left-btn').addEventListener('click', () => {
     currentSlide--;
-    if (currentSlide < 0) currentSlide = 0;  
+    if (currentSlide < 0) currentSlide = movies.length - 1;  
     slider.style.transform = `translateX(-${currentSlide * 850}px)`;  
 });
+function updateFirstMovieCard() {
+  const item = movies[0];  
+  const card = document.querySelector('.trending-movie-card1');  
+  card.querySelector('h1').textContent = item.title;
+  card.querySelector('.trending-movie-details span').textContent = `★★★★★ ${item.vote_average}`;
+  card.querySelector('.trending-movie-details span:nth-child(2)').textContent = item.runtime ? `| ${item.runtime} mins` : '| Runtime unknown';
+  card.querySelector('p').textContent = item.overview;
+  card.querySelector('img').src = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+}
 
 fetch(url, {
     headers: {
@@ -27,10 +37,11 @@ fetch(url, {
     .then(response => response.json())
     .then(data => {
       movies = data.results.slice(0, 20);  
+      updateFirstMovieCard();
       movies.map((movie,index) => {
-        addMovieToSlider(index);   
-      })
-    })
+        addMovieToSlider(index+1);
+        document.querySelector('#tm-slider').classList.add('visible');
+      })})
     .catch(err => console.error('Error fetching data:', err));
 
 function addMovieToSlider(index) {
@@ -44,21 +55,23 @@ function addMovieToSlider(index) {
     card.querySelector('.trending-movie-details span:nth-child(2)').textContent = item.runtime ? `| ${item.runtime} mins` : '| Runtime unknown';
     card.querySelector('p').textContent = item.overview;  
     card.querySelector('img').src = `https://image.tmdb.org/t/p/w500${item.poster_path}`;  
-    card.querySelector('.control-right-btn').addEventListener('click', () => {
+    card.querySelector('.control-right-btn').
+    
+  addEventListener('click', () => {
       currentSlide++;
-      if (currentSlide > movies.length - 1) currentSlide = movies.length - 1;
+      if (currentSlide > movies.length - 1) currentSlide = 0;
       slider.style.transform = `translateX(-${currentSlide * 850}px)`;
   });
 
   card.querySelector('.control-left-btn').addEventListener('click', () => {
       currentSlide--;
-      if (currentSlide < 0) currentSlide = 0;
+      if (currentSlide < 0) currentSlide = movies.length - 1;
       slider.style.transform = `translateX(-${currentSlide * 850}px)`;
   });
     tmSlider.appendChild(card); 
 }
 
-
+//END-Cerenucakb/BE-12-Create-Trending-Movies-Section
 //Melek/BE-20/Implement the FAQ section/JavaScript/Start
 const akordiyon = document.getElementsByClassName("content-box");
 
